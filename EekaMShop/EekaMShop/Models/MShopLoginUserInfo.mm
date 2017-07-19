dd30171a4c3e81e9d451e57335e51ce3e3ba613b
mafanghua <980992720@qq.com>
2017-07-19 16:28:55 +0800

    no message

--- EekaMShop/EekaMShop/Models/MShopLoginUserInfo.mm
+++ EekaMShop/EekaMShop/Models/MShopLoginUserInfo.mm
@@ -18,7 +18,24 @@
 
 WCDB_IMPLEMENTATION(MShopLoginUserInfo)
 
+WCDB_SYNTHESIZE(MShopLoginUserInfo, userId)
 WCDB_SYNTHESIZE(MShopLoginUserInfo, token)
+WCDB_SYNTHESIZE(MShopLoginUserInfo, avatar)
+WCDB_SYNTHESIZE(MShopLoginUserInfo, department)
+WCDB_SYNTHESIZE(MShopLoginUserInfo, email)
+WCDB_SYNTHESIZE(MShopLoginUserInfo, englishName)
+WCDB_SYNTHESIZE(MShopLoginUserInfo, extattr)
+WCDB_SYNTHESIZE(MShopLoginUserInfo, gender)
+WCDB_SYNTHESIZE(MShopLoginUserInfo, isLeader)
+WCDB_SYNTHESIZE(MShopLoginUserInfo, mobile)
+WCDB_SYNTHESIZE(MShopLoginUserInfo, name)
+WCDB_SYNTHESIZE(MShopLoginUserInfo, position)
+WCDB_SYNTHESIZE(MShopLoginUserInfo, status)
+WCDB_SYNTHESIZE(MShopLoginUserInfo, telephone)
+WCDB_SYNTHESIZE_DEFAULT(MShopLoginUserInfo, timestamp, WCTDefaultTypeCurrentTimestamp)
+
+
+WCDB_PRIMARY(MShopLoginUserInfo, userId)
 
 
 @end
