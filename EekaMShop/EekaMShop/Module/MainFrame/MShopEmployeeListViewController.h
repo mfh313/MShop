79bad2787e8144f7594eddbcc18aa0cb89950179
mafanghua <980992720@qq.com>
2017-07-19 18:03:17 +0800

    no message

---
+++ EekaMShop/EekaMShop/Module/MainFrame/MShopEmployeeListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MShopEmployeeListViewController.h
+//  EekaMShop
+//
+//  Created by EEKA on 2017/7/19.
+//  Copyright © 2017年 eeka. All rights reserved.
+//
+
+#import "MMViewController.h"
+
+@interface MShopEmployeeListViewController : MMViewController
+
+@end
---
+++ EekaMShop/EekaMShop/Module/MainFrame/MShopEmployeeListViewController.m
@@ -0,0 +1,30 @@
+//
+//  MShopEmployeeListViewController.m
+//  EekaMShop
+//
+//  Created by EEKA on 2017/7/19.
+//  Copyright © 2017年 eeka. All rights reserved.
+//
+
+#import "MShopEmployeeListViewController.h"
+
+@interface MShopEmployeeListViewController ()
+
+@end
+
+@implementation MShopEmployeeListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"会员列表";
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+@end
