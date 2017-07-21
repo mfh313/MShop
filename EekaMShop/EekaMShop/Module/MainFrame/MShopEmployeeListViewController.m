030a7354e4bf17a592c0e0af0ece0deea2ccb7cc
mafanghua <980992720@qq.com>
2017-07-21 10:19:58 +0800

    no message

--- EekaMShop/EekaMShop/Module/MainFrame/MShopEmployeeListViewController.m
+++ EekaMShop/EekaMShop/Module/MainFrame/MShopEmployeeListViewController.m
@@ -11,6 +11,9 @@
 #import "MShopGetEmployeeListApi.h"
 
 @interface MShopEmployeeListViewController ()
+{
+    
+}
 
 @end
 
@@ -21,6 +24,33 @@
     
     self.title = @"会员列表";
     
+    [self getEmployeeList];
+}
+
+-(void)getEmployeeList
+{
+    __weak typeof(self) weakSelf = self;
+    
+    MShopGetEmployeeListApi *getEmployeeListApi = [MShopGetEmployeeListApi new];
+    getEmployeeListApi.deptId = @"13041";
+    getEmployeeListApi.animatingText = @"正在获取用户列表...";
+    getEmployeeListApi.animatingView = MFAppWindow;
+    [getEmployeeListApi startWithCompletionBlockWithSuccess:^(YTKBaseRequest * request) {
+        
+        if (!getEmployeeListApi.messageSuccess) {
+            [self showTips:getEmployeeListApi.errorMessage];
+            return;
+        }
+        
+//        __strong typeof(weakSelf) strongSelf = weakSelf;
+        
+        
+    } failure:^(YTKBaseRequest * request) {
+        
+        NSString *errorDesc = [NSString stringWithFormat:@"错误状态码=%@\n错误原因=%@",@(request.error.code),[request.error localizedDescription]];
+        [self showTips:errorDesc];
+    }];
+
 }
 
 - (void)didReceiveMemoryWarning {
